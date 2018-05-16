b3254195c4078c3711f0d31a761d2f3d82f88f92
MaQi <383930056@qq.com>
2018-05-16 17:09:29 +0800

    PHP 配合 Hbuilder  实现跨域Ajax后回显JSON

--- MuchTp5_try/application/api/controller/Name.php
+++ MuchTp5_try/application/api/controller/Name.php
@@ -35,20 +35,7 @@ class Name extends Controller
         return $this->fetch('index');
     }
 
-    public function run(&$dispatch){
-        $host_name = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "*";
-        $headers = [
-            "Access-Control-Allow-Origin" => $host_name,
-            "Access-Control-Allow-Credentials" => 'true',
-            "Access-Control-Allow-Headers" => "x-token,x-uid,x-token-check,x-requested-with,content-type,Host"
-        ];
-        if($dispatch instanceof Response) {
-            $dispatch->header($headers);
-        } else if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            $dispatch['type'] = 'response';
-            $response = new Response('', 200, $headers);
-            $dispatch['response'] = $response;
-        }
+    public function run(){
         $rs = db('forum_posts')->select();
         $rs1 = json(0, '数据返回成功', 1000, $rs);
         return $rs1;
